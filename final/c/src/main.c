359af596f6ef37ba4409bf4d9c19839b440b1c56
mikeyQwn <mikeyQwn@gmail.com>
2024-09-23 22:34:39 +0300

    Add a lookup table

--- final/c/src/main.c
+++ final/c/src/main.c
@@ -3,6 +3,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+uint8_t get_type_by_cmps(uint8_t a, uint8_t b, uint8_t c) {
+    const uint8_t LOOKUP[3][3][3] = {
+        {
+            {2, 2, 2},
+            {4, 4, 4},
+            {9, 8, 10},
+        },
+        {
+            {5, 5, 5},
+            {3, 3, 3},
+            {6, 6, 6},
+        },
+        {
+            {12, 11, 13},
+            {7, 7, 7},
+            {1, 1, 1},
+        },
+    };
+    return LOOKUP[a][b][c];
+}
+
+uint8_t alternative_lookup(uint8_t a, uint8_t b, uint8_t c) {
+    const uint8_t LOOKUP[27] = {
+        2, 2, 2, 4, 4,  4,  9,  8, 10, 5, 5, 5, 3, 3,
+        3, 6, 6, 6, 12, 11, 13, 7, 7,  7, 1, 1, 1,
+    };
+    const uint8_t a9 = a + a + a + a + a + a + a + a + a;
+    const uint8_t b3 = b + b + b;
+    return LOOKUP[a9 + b3 + c];
+}
+
 long get_file_size(FILE *f) {
     long current_position = ftell(f);
     if (current_position < 0) {
@@ -26,7 +57,26 @@ long get_file_size(FILE *f) {
     return file_size;
 }
 
+int test_fnx() {
+    for (int a = 0; a < 3; ++a) {
+        for (int b = 0; b < 3; ++b) {
+            for (int c = 0; c < 3; ++c) {
+                if (get_type_by_cmps(a, b, c) != alternative_lookup(a, b, c)) {
+                    printf("a: %d, b: %d, c: %d", a, b, c);
+                    return 1;
+                }
+            }
+        }
+    }
+    printf("[info] All done!\n");
+    return 0;
+}
+
 int main() {
+    if (test_fnx() != 0) {
+        return 1;
+    }
+
     const char input_path[] = "input.bin";
 
     FILE *handle = fopen(input_path, "r");
