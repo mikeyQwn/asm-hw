902dd9102be30807eb60113f145047684fc3fff0
mikeyQwn <mikeyQwn@gmail.com>
2024-09-29 12:57:45 +0300

    Finalize report

--- final/asm/05-wave-class.asm
+++ final/asm/05-wave-class.asm
@@ -1,3 +1,5 @@
+; Задача - классифицировать количество замеров по трем точкам
+
 format PE console
 
 entry start
@@ -5,6 +7,7 @@ include 'C:\fasm\INCLUDE\win32a.inc'
 
 section '.data' readable writeable
         filename db 'input.bin', 0
+        out_filename db 'output.bin', 0
         failed_to_open_file_msg db 'Failed to open the file! Error code: %d', 0xA, 0x0
         failed_to_allocate_memory_msg db 'Failed to allocate memory for the file content buffer', 0xA, 0x0
         file_size_fmt db 'File size: %d bytes', 0xA, 0x0
@@ -14,55 +17,65 @@ section '.data' readable writeable
 
 section '.bss' readable writeable
         h_file dd ?
+        h_out_file dd ?
         file_size dd ?
         file_buffer dd ?
         out_buffer dd ?
+        out_buffer_len dd ?
         bytes_read dd ?
-        h_lib dd ?
 
 
 section '.code' code readable executable
 start:
-        invoke CreateFileA, filename, GENERIC_READ, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0
+    ; Открытие файла с входными значениями с именем `input.bin`, сохранение handle
+    invoke CreateFileA, filename, GENERIC_READ, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0
         mov [h_file], eax
 
         cmp [h_file], INVALID_HANDLE_VALUE 
         je file_error
 
-        invoke GetFileSize, [h_file], 0
+    ; Получение размера файла 
+    invoke GetFileSize, [h_file], 0
         mov [file_size], eax
 
-
-        invoke VirtualAlloc, 0, [file_size], MEM_COMMIT, PAGE_READWRITE
+    ; Выделение памяти для массива, в который запишутся байты файла
+    invoke VirtualAlloc, 0, [file_size], MEM_COMMIT, PAGE_READWRITE
         mov [file_buffer], eax
 
         mov eax, [file_size]
         shr eax, 3
-        invoke VirtualAlloc, 0, eax, MEM_COMMIT, PAGE_READWRITE
+        mov [out_buffer_len], eax
+
+    ; Выделение памяти для массива, в который запишутся байты выходного файла
+    invoke VirtualAlloc, 0, eax, MEM_COMMIT, PAGE_READWRITE
         mov [out_buffer], eax
 
         cmp eax, 0
-        je mem_error
+    je mem_error
 
-        invoke ReadFile, [h_file], [file_buffer], [file_size], bytes_read, 0
+    ; Чтение файла с входными значениями
+    invoke ReadFile, [h_file], [file_buffer], [file_size], bytes_read, 0
         cmp eax, 0
-        je read_error
+    je read_error
 
-        invoke printf, file_size_fmt, [file_size]
+    ; Распечатывание размера входного файла в байтах
+    invoke printf, file_size_fmt, [file_size]
 
-        invoke CloseHandle, [h_file]
+    ; Зактыие handle входного файла 
+    invoke CloseHandle, [h_file]
 
-        invoke Wc, [file_buffer], [file_size], out_buffer
+    ; Общащение к dll
+    invoke Wc, [file_buffer], [file_size], [out_buffer]
 
-        mov ecx, [file_size]
-        shr ecx, 3
-
+    ; Распечатка выходного массива
+        mov ecx, [out_buffer_len]
 		xor ebx, ebx
 print_loop:
-		movzx eax, byte[out_buffer + ebx]
+        mov eax, [out_buffer]
+		movzx eax, byte[eax + ebx]
 		push ecx
 
-		invoke printf, index_value_fmt, ebx, eax
+    invoke printf, index_value_fmt, ebx, eax
 		add esp, 12
 
 		pop ecx
@@ -70,26 +83,35 @@ print_loop:
 
 
 		cmp ebx, ecx
-		jl print_loop
+    jl print_loop
+
+    ; Создание выходного файла с именем `output.bin`
+    invoke CreateFileA, out_filename, GENERIC_WRITE, 0, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0
+        mov [h_out_file], eax
+        cmp [h_out_file], INVALID_HANDLE_VALUE 
+    je file_error
+
+    ; Запись выходного массива в файл
+    invoke WriteFile, [h_out_file], [out_buffer], [out_buffer_len], 0, 0
+
+    ; Закрытие выходного файла
+    invoke CloseHandle, [h_out_file]
 
 exit:
-        invoke ExitProcess, 0
-
-free_library:
-        invoke FreeLibrary, [h_lib]
+    invoke ExitProcess, 0
 
 file_error:
-        invoke GetLastError
-        invoke printf, failed_to_open_file_msg, eax
-        invoke ExitProcess, 1
+    invoke GetLastError
+    invoke printf, failed_to_open_file_msg, eax
+    invoke ExitProcess, 1
 
 mem_error:
-        invoke printf, failed_to_allocate_memory_msg
-        invoke ExitProcess, 1
+    invoke printf, failed_to_allocate_memory_msg
+    invoke ExitProcess, 1
 
 read_error:
-        invoke printf, failed_to_read_file_msg
-        invoke ExitProcess, 1
+    invoke printf, failed_to_read_file_msg
+    invoke ExitProcess, 1
 
 
 section '.idata' import data readable writeable
@@ -107,6 +129,7 @@ import kernel32, \
     FreeLibrary, 'FreeLibrary', \
     GetProcAddress, 'GetProcAddress', \
     GetLastError, 'GetLastError', \
+    WriteFile, 'WriteFile', \
     ExitProcess, 'ExitProcess'
 
 import msvcrt, \
--- final/asm/wcdll.asm
+++ final/asm/wcdll.asm
@@ -1,174 +1,185 @@
+; Задача - классифицировать количество замеров по трем точкам
+
 format PE console DLL
 entry DllEntryPoint
 
 include 'C:\fasm\INCLUDE\win32a.inc'
 
 section '.data' readable writeable
-    out_buf_size dd 0
-    max_iter dd 0
-	current_position dd 0
+        max_iter dd 0
+        current_position dd 0
 
-	first dw 0
-	second dw 0
-	third dw 0
+        first dw 0
+        second dw 0
+        third dw 0
 
-	first_cmp dw 0
-	second_cmp dw 0
-	third_cmp dw 0
+        first_cmp dw 0
+        second_cmp dw 0
+        third_cmp dw 0
 
-	current_type dd 0
+        current_type dd 0
 
-	lookup db 2, 2, 2, 4, 4, 4,  9,  8, 10, 5, 5, 5, 3, 3, 3, 6, 6, 6, 12, 11, 13, 7, 7,  7, 1, 1, 1
-
-	print_fmt db 'Idx: %d, Ty: %d, f: %d, s: %d, t: %d', 0xA, 0
+        ; Трехмерный массив со всеми классами сравниваемых чисел
+        ; -1 отмечены ситуации, невозможные при трех сравненияих 3-х чисел
+        lookup db  \
+         -1, -1,  2, \   ; < < <, < < =, < < >
+         -1, -1,  4, \   ; < = <, < = =, < = >
+          9,  8, 10, \   ; < > <, < > =, < > >
+         \
+         -1, -1,  5, \  ; = < <, = < =, = < >
+         -1,  3, -1, \  ; = = <, = = =, = = >
+          6, -1, -1, \  ; = > <, = > =, = > >
+         \
+         12, 11, 13, \  ; > < <, < < =, > < >
+          7, -1, -1, \  ; > = <, < = =, > = >
+          1, -1, -1     ; > > <, < > =, > > >
 
 section '.text' code readable executable
 
+; Входная точка в DLL
 proc DllEntryPoint hinstDLL,fdwReason,lpvReserved
-	mov	eax, 1
+        mov	eax, 1
 	ret
 endp
 
 ; eax - a, ebx - b, ecx - c
+; Возврвщает класс номеров отношений трех чисел A, B, C
+; eax - первое отношение (между A и B)
+; ebx - второе отношение (между B и C)
+; ecx - трертье отношение (между C и A)
+; Отношения двух чисел a и b представляются как
+; 0 при a < b
+; 1 при a = b
+; 2 при a > b
 get_type:
-	movzx eax, al
-	lea eax, [eax * 8 + eax]
+        movzx eax, al
+        lea eax, [eax * 8 + eax] ; (eax = (al << 3) + al = 3 * 3 * al )
 
-	movzx ebx, bl
-	lea ebx, [ebx * 2 + ebx]
+        movzx ebx, bl
+        lea ebx, [ebx * 2 + ebx] ; (ebx = (bl << 1) + al = 3 * bl )
 
-	movzx ecx, cl
+        movzx ecx, cl
 
-	add eax, ebx
-	add eax, ecx
+        add eax, ebx 
+        add eax, ecx
+        ; (eax = 9al + 3bl + cl)
 
-	movzx eax, byte [lookup + eax]
+        movzx eax, byte [lookup + eax]
+        ; eax = lookup[al][bl][cl]
 	ret
 
-; eax - a, ebx - b
+; Возвращает численное представление отношения между a и b
+; eax - a 
+; ebx - b
+; Отношения двух чисел a и b представляются как
+; 0 при a < b
+; 1 при a = b
+; 2 при a > b
 get_cmp:
-	movzx eax, ax
-	movzx ebx, bx
+        movzx eax, ax
+        movzx ebx, bx
 
-	cmp eax, ebx
+        cmp eax, ebx
     jb  less
     je  equal   
 
-	mov eax, 2
+        mov eax, 2
 	ret
 
 less:
-	xor eax, eax
+        xor eax, eax
 	ret
 
 equal:
-	mov al, 1
+        mov al, 1
 	ret
 
-
+; Процедура для классификаций отношений
+; in_buf - указатель на входной массив
+; buf_size - размер входного массива 
+; out_buf - указатель на выходной массив
 proc Wc in_buf, buf_size, out_buf
-    mov eax, [buf_size]
-    mov [out_buf_size], eax 
-    shr [out_buf_size], 3
+; Запись масимального возможного значения,
+; по которому не произойдет выход за границы массива
+        mov eax, [buf_size]
+        mov [max_iter], eax 
+        sub [max_iter], 12
 
-    mov eax, [buf_size]
-	mov [max_iter], eax 
-	sub [max_iter], 8
-
-	xor ecx, ecx
-	mov [current_position], ecx
-
+        xor ecx, ecx
+        mov [current_position], ecx
 mainloop:
-	mov ebx, [in_buf]
-	; Первое число
-	mov [first], 0
-	movzx eax, byte[ebx + ecx + 2]
-	mov [first], ax
-	movzx eax, byte[ebx + ecx + 3]
-	add [first], ax
+        mov ebx, [in_buf]
+        ; Первое число
+        mov [first], 0
+        movzx eax, byte[ebx + ecx + 2]
+        mov [first], ax
+        movzx eax, byte[ebx + ecx + 3]
+        add [first], ax
 
-	; Второе число
-	mov [second], 0
-	movzx eax, byte[ebx + ecx + 6]
-	mov [second], ax
-	movzx eax, byte[ebx + ecx + 7]
-	add [second], ax
+        ; Второе число
+        mov [second], 0
+        movzx eax, byte[ebx + ecx + 6]
+        mov [second], ax
+        movzx eax, byte[ebx + ecx + 7]
+        add [second], ax
 
-	; Третье число
-	mov [third], 0
-	movzx eax, byte[ebx + ecx + 10]
-	mov [third], ax
-	movzx eax, byte[ebx + ecx + 11]
-	add [third], ax
+        ; Третье число
+        mov [third], 0
+        movzx eax, byte[ebx + ecx + 10]
+        mov [third], ax
+        movzx eax, byte[ebx + ecx + 11]
+        add [third], ax
 
-	push ecx
+        push ecx
 
-	movzx eax, [first]
-	movzx ebx, [second]
+    ; Получение численного представления отношений первого и второого числа
+        movzx eax, [first]
+        movzx ebx, [second]
 	call get_cmp
-	mov [first_cmp], ax
+        mov [first_cmp], ax
 
-	movzx eax, [second]
-	movzx ebx, [third]
+    ; Получение численного представления отношений второого и третьего числа
+        movzx eax, [second]
+        movzx ebx, [third]
 	call get_cmp
-	mov [second_cmp], ax
+        mov [second_cmp], ax
 
-	movzx eax, [third]
-	movzx ebx, [first]
+    ; Получение численного представления отношений третьего и первого числа
+        movzx eax, [third]
+        movzx ebx, [first]
 	call get_cmp
-	mov [third_cmp], ax
+        mov [third_cmp], ax
 
-	movzx eax, [first_cmp]
-	movzx ebx, [second_cmp]
-	movzx ecx, [third_cmp]
-
+    ; Получение номера отношений трех чисел
+        movzx eax, [first_cmp]
+        movzx ebx, [second_cmp]
+        movzx ecx, [third_cmp]
 	call get_type
-	mov [current_type], eax
-	mov ebx, [out_buf]
-	mov ecx, [current_position]
-	mov byte [ebx+ecx], al
+        mov [current_type], eax
+        mov ebx, [out_buf]
+        mov ecx, [current_position]
+        mov byte [ebx+ecx], al
 
-	add [current_position], 1
+        add [current_position], 1
+        pop ecx
 
-	;push eax
-	;push ebx
-	;push ecx
-	;push edx
-	;
-	;movzx ebx, [first_cmp]
-	;movzx ecx, [second_cmp]
-	;movzx edx, [third_cmp]
-	;
-	;invoke printf, print_fmt, [current_position], [current_type], ebx, ecx, edx
-	;add esp, 24
-	;
-	;pop edx
-	;pop ecx
-	;pop ebx
-	;pop eax
+    ; Пропуск двух записей сразу, чтобы не было перекрытия
+        add ecx, 8
 
-	pop ecx
-
-	add ecx, 8
-
-	cmp ecx, [max_iter]
+        cmp ecx, [max_iter]
 	jl mainloop
 
-    mov eax, [current_position]
+        mov eax, [current_position]
 	ret
 endp
 
 section '.idata' import data readable writeable
   library kernel, 'kernel32.dll', \
-	  user, 'user32.dll', \
-	  msvcrt, 'msvcrt.dll'
+	  user, 'user32.dll'
 
 section '.edata' export data readable
   export 'wcdll.dll', \
 	 Wc, 'Wc'
-
-import msvcrt, \
-    printf, 'printf'
 
 section '.reloc' fixups data readable discardable
   if $=$$
